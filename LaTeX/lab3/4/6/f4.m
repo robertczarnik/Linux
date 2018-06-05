af5bd34796b3fe1b7c7115bce2ae0fd95f93e938
robertczarnik <robert.czarnik98@gmail.com>
2018-06-05 15:14:15 +0200

    latex

---
+++ LaTeX/lab3/4/6/f4.m
@@ -0,0 +1,8 @@
+t = linspace(0, 2*pi, 81);
+p = linspace(0, pi/2, 21);
+[tt,pp] = meshgrid(t, p);
+r = 2 * cos(pp);
+x = r .* sin(pp) .* cos(tt);
+y = r .* sin(pp) .* sin(tt);
+z = r .* sin(pp);
+mesh(x,y,z) 
